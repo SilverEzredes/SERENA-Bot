cc833b027c90425c7c2e72cc2bb9095ff65c3af3
SilverEzredes <68148231+SilverEzredes@users.noreply.github.com>
2021-09-17 00:59:21 +0200

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -44,19 +44,19 @@ class Fun(commands.Cog,
                                     title=f"🌿 Herb!",
                                     description=f"<@!{ctx.author.id}> just rolled you a G+G+G blunt of the greenest herb🌿!\n"
                                                 f"A mixture of 3 Green Herbs. Completely restores health.",
-                                    thumbnail="https://cdn.discordapp.com/emojis/762601822979358751.png?v=1")
+                                    thumbnail="https://cdn.discordapp.com/emojis/762601822979358751.png")
         else:
             await utils.embed_reply(ctx,
                                     title=f"🌿 Herb!",
                                     description=f"<@!{ctx.author.id}> found some funny looking plant and decided to smoke it🌿!\n"
                                                 f"Herb that restores partial health.",
-                                    thumbnail="https://cdn.discordapp.com/emojis/762601822979358751.png?v=1")
+                                    thumbnail="https://cdn.discordapp.com/emojis/762601822979358751.png")
 
-    @commands.command(name="burrito",
-                      description="Deliver someone a SpaceBurrito (cosmetic, no rewards)",
-                      usage="{prfx}burrito [ user ]",
-                      help="user: the user to give a burrito to (ping, name, id) (optional)",
-                      aliases=["spaceburrito", "galacticburrito"])
+    @commands.command(name="choco bar",
+                      description="Treat someone with a delicious SewerChocolate™️ (cosmetic, no rewards)",
+                      usage="{prfx}choco bar [ user ]",
+                      help="user: the user to give a choco bar to (ping, name, id) (optional)",
+                      aliases=["chocolate bar", "sewerchocolate", "choco"])
     async def burrito(self, ctx, target: Union[discord.Member, discord.User, int, str] = None):
         # Convert target input to discord.Member
         if not target:
@@ -82,16 +82,16 @@ class Fun(commands.Cog,
         if target.id != ctx.author.id:
             await utils.embed_reply(ctx,
                                     content=f"<@!{target.id}>",
-                                    title=f"🌯 SpaceBurrito!",
-                                    description=f"<@!{ctx.author.id}> just delivered you a delicious Space🌌Burrito🌯!\n"
-                                                f"Eat it before it gets cold!",
-                                    thumbnail="https://cdn.discordapp.com/emojis/779465397903949825.png")
+                                    title=f"🍫 Sewer Chocolate™️!",
+                                    description=f"<@!{ctx.author.id}> just delivered you a delicious 🍫 Sewer Chocolate™️🍫!\n"
+                                                f"Just don't look at the expiration date...",
+                                    thumbnail="https://cdn.discordapp.com/emojis/888191417556561980.png")
         else:
             await utils.embed_reply(ctx,
-                                    title=f"🌯 SpaceBurrito!",
-                                    description=f"<@!{ctx.author.id}> doesn't have any friends yet so they bought themselves a delicious Space🌌Burrito🌯!\n"
-                                                f"Time to eagerly enjoy it in complete silence!",
-                                    thumbnail="https://cdn.discordapp.com/emojis/779465397903949825.png")
+                                    title=f"🍫 Sewer Chocolate™️!",
+                                    description=f"<@!{ctx.author.id}> found some 🍫 chocolate bar looking thing while exploring the sewers.\n"
+                                                f"Are you really considering eating that?",
+                                    thumbnail="https://cdn.discordapp.com/emojis/888191417556561980.png")
 
 
 def setup(bot):
