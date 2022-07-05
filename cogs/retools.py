dc923b9d2d891842b748b0abf0daca625795a6ce
SilverEzredes <68148231+SilverEzredes@users.noreply.github.com>
2022-07-05 12:32:09 +0200

    re + DEPLOY

--- cogs/retools.py
+++ cogs/retools.py
@@ -8,23 +8,33 @@ import os
 from modules import globals, utils
 
 
-class Tools(commands.Cog,
-          description="Commands for RE Engine Tools"):
+class REtools(commands.Cog,
+          description="Commands for RE Engine tools"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="reframework",
-                      description="A mod framework, scripting platform, and modding tool for RE Engine games.",
-                      usage="{prfx}reframework",
+    @commands.command(name="REFramework",
+                      description="",
+                      usage="{prfx}REFramework",
                       help="",
                       aliases=["ref"])
     async def reframework(self, ctx):
-        desc = "A mod framework, scripting platform, and modding tool for RE Engine games.\n"
+        desc = "A mod framework, scripting platform, and modding tool for RE Engine games."
         await utils.embed_reply(ctx,
                                 title="REFramework",
-                                description=desc
-                                thumbnail=globals.bot.user.avatar_url),
-
+                                description=desc,
+                                fields=[
+                                    ["♾️ Uptime:",          f"{utils.time_from_start()}",                                                                                                                                          True],
+                                    ["☯️ Next Restart In:", f"{utils.time_to_restart()}",                                                                                                                                          True],
+                                    ["⏳ Ping:",            f"{int(globals.bot.latency * 1000)}ms",                                                                                                                                True],
+                                    ["📟 CPU Usage",        f"{psutil.cpu_percent()}%",                                                                                                                                            True],
+                                    ["💾 RAM Usage",        f"{utils.pretty_size(psutil.Process(os.getpid()).memory_info().rss)}/{'512MB' if os.environ.get('DYNO') else utils.pretty_size(psutil.virtual_memory().total)}",       True],
+                                    ["🚀 Last Update",      f"{datetime.datetime.fromisoformat(os.environ.get('HEROKU_RELEASE_CREATED_AT')[:-1]).strftime('%d/%m/%Y') if os.environ.get('HEROKU_RELEASE_CREATED_AT') else 'N/A'}", True],
+                                    ["👨‍💻 Developer",        "[WillyJL](https://linktr.ee/WillyJL)",                                                                                                                               True],
+                                    ["📚 Library",          f"discord.py v{discord.__version__}",                                                                                                                                  True],
+                                    ["📦 Version",          f"{os.environ.get('HEROKU_RELEASE_VERSION') if os.environ.get('HEROKU_RELEASE_VERSION') else 'N/A'}",                                                                  True],
+                                ],
+                                thumbnail=globals.bot.user.avatar_url)
 
 def setup(bot):
-    bot.add_cog(Tools(bot))
+    bot.add_cog(REtools(bot))
