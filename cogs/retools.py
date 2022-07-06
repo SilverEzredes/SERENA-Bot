3571525b37806625f939517ee9a76bf25a9213d8
SilverEzredes <68148231+SilverEzredes@users.noreply.github.com>
2022-07-06 09:09:09 +0200

    More Tools Vol.2 + DEPLOY

--- cogs/retools.py
+++ cogs/retools.py
@@ -30,7 +30,7 @@ class REtools(commands.Cog,
                                 ],
                                 thumbnail="https://cdn.discordapp.com/emojis/994126039246700544.png")
 
-    @commands.command(name="Noesis Plugin & REEM",
+    @commands.command(name="Noesis_Plugin_REEM",
                       description="",
                       usage="{prfx}noesisplugin",
                       help="",
@@ -78,7 +78,7 @@ class REtools(commands.Cog,
                                 ],
                                 thumbnail="https://cdn.discordapp.com/emojis/994126946680176751.png")
 
-    @commands.command(name="010 - RE RSZ",
+    @commands.command(name="010--RE_RSZ",
                       description="",
                       usage="{prfx}re_rsz",
                       help="",
@@ -95,7 +95,7 @@ class REtools(commands.Cog,
                                 ],
                                 thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
 
-    @commands.command(name="3DSmax - RE-Engine Mesh Tool",
+    @commands.command(name="3DSmax--RE-Engine_Mesh_Tool",
                       description="",
                       usage="{prfx}re_3dsmaxMesh",
                       help="",
@@ -111,7 +111,7 @@ class REtools(commands.Cog,
                                 ],
                                 thumbnail="https://cdn.discordapp.com/emojis/994125244212199424.png")
 
-    @commands.command(name="3DSmax - alphaZomega Tool",
+    @commands.command(name="3DSmax--alphaZomega_Tool",
                       description="",
                       usage="{prfx}alphaZtool",
                       help="",
@@ -125,9 +125,9 @@ class REtools(commands.Cog,
                                     ["🔗 Link:",          "[alphaZomega Tool - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932645657364287519)",                                                                                                                                          True],
                                     ["💻 Developer", "alphaZomega",                                                                                                                                          True],
                                 ],
-                                thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
+                                thumbnail="https://cdn.discordapp.com/emojis/994125244212199424.png")
 
-    @commands.command(name="3DSmax - ResetMesh",
+    @commands.command(name="3DSmax--ResetMesh",
                       description="",
                       usage="{prfx}resetmesh",
                       help="",
@@ -141,9 +141,9 @@ class REtools(commands.Cog,
                                     ["🔗 Link:",          "[ResetMesh - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932645788826353705)",                                                                                                                                          True],
                                     ["💻 Developer", "alphaZomega",                                                                                                                                          True],
                                 ],
-                                thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
+                                thumbnail="https://cdn.discordapp.com/emojis/994125244212199424.png")
 
-    @commands.command(name="010 - Offset Fixer",
+    @commands.command(name="010--Offset_Fixer",
                       description="",
                       usage="{prfx}offsetfixer",
                       help="",
@@ -157,6 +157,102 @@ class REtools(commands.Cog,
                                     ["🔗 Link:",          "[Alpha's Offset Fixer - GitHub](https://github.com/alphazolam/Alpha-Offset-Fixer)",                                                                                                                                          True],
                                     ["💻 Developer", "alphaZomega",                                                                                                                                          True],
                                     ["📖 More Info:",       "[Offset Fixer - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932646362795868240)",                                                                                                                                           True],
+                                ],
+                                thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
+
+    @commands.command(name="3DSmax--Motlist_Tool",
+                      description="",
+                      usage="{prfx}motlistTool",
+                      help="",
+                      aliases=["3dsmot", "3dsmotlist", "MSmot"])
+    async def MotlistTool(self, ctx):
+        desc = "The motlist tool is a Maxscript that can export animations imported by RevilMax back to the game format, for full animation modding."
+        await utils.embed_reply(ctx,
+                                title="Motlist Tool",
+                                description=desc,
+                                fields=[
+                                    ["🔗 Link:",          "[Motlist Tool - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932646688370331768)",                                                                                                                                          True],
+                                    ["💻 Developer", "alphaZomega",                                                                                                                                          True],
+                                ],
+                                thumbnail="https://cdn.discordapp.com/emojis/994125244212199424.png")
+
+    @commands.command(name="010--Motlist",
+                      description="",
+                      usage="{prfx}MotlistTemp",
+                      help="",
+                      aliases=["010mot", "010motlist", "motlistbt", "motbt"])
+    async def MotlistTemplate(self, ctx):
+        desc = "The motlist template is a helpful tool for editing RE Engine animations and their associated actions and effects."
+        await utils.embed_reply(ctx,
+                                title="Motlist Template",
+                                description=desc,
+                                fields=[
+                                    ["🔗 Link:",          "[Motlist Template - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932647093208760330)",                                                                                                                                          True],
+                                    ["💻 Developers", "alphaZomega, che, Jackal",                                                                                                                                          True],
+                                ],
+                                thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
+
+    @commands.command(name="010--Motbank",
+                      description="",
+                      usage="{prfx}MotbankTemp",
+                      help="",
+                      aliases=["010bank", "010motbank", "motbankbt", "bankbt"])
+    async def MotbankTemplate(self, ctx):
+        desc = "The motbank template is used to edit motbank files from all RE Engine games."
+        await utils.embed_reply(ctx,
+                                title="Motbank Template",
+                                description=desc,
+                                fields=[
+                                    ["🔗 Link:",          "[Motbank Template - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932647199974752256)",                                                                                                                                          True],
+                                    ["💻 Developer", "alphaZomega",                                                                                                                                          True],
+                                ],
+                                thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
+
+    @commands.command(name="010--Mesh",
+                      description="",
+                      usage="{prfx}MeshTemp",
+                      help="",
+                      aliases=["010mesh", "meshbt"])
+    async def MeshTemplate(self, ctx):
+        desc = "The MESH template can edit RE Engine MESH model files, allowing you to do such things as edit/rename individual bones."
+        await utils.embed_reply(ctx,
+                                title="Mesh Template",
+                                description=desc,
+                                fields=[
+                                    ["🔗 Link:",          "[Mesh Template - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932647646244507679)",                                                                                                                                          True],
+                                    ["💻 Developers", "che, alphaZomega, Darkness",                                                                                                                                          True],
+                                ],
+                                thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
+
+    @commands.command(name="010--MDF",
+                      description="",
+                      usage="{prfx}MDFTemp",
+                      help="",
+                      aliases=["010mdf", "mdfbt"])
+    async def MDFTemplate(self, ctx):
+        desc = "The MDF template allows you to edit the texture locations, material flags and parameters of each material."
+        await utils.embed_reply(ctx,
+                                title="MDF Template",
+                                description=desc,
+                                fields=[
+                                    ["🔗 Link:",          "[MDF Template - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932648376221171832)",                                                                                                                                          True],
+                                    ["💻 Developers", "che, alphaZomega, Darkness",                                                                                                                                          True],
+                                ],
+                                thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
+
+    @commands.command(name="010--Chain",
+                      description="",
+                      usage="{prfx}ChainTemp",
+                      help="",
+                      aliases=["010chain", "chainbt"])
+    async def ChainTemplate(self, ctx):
+        desc = "The Chain template can be used to edit the properties of RE Engine chain physics files."
+        await utils.embed_reply(ctx,
+                                title="Chain Template",
+                                description=desc,
+                                fields=[
+                                    ["🔗 Link:",          "[Chain Template - Thread](https://discord.com/channels/718224210270617702/930092288330309683/932648576398540860)",                                                                                                                                          True],
+                                    ["💻 Developer", "alphaZomega",                                                                                                                                          True],
                                 ],
                                 thumbnail="https://cdn.discordapp.com/emojis/955607176674697276.png")
 
